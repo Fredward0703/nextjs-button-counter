565c7867e8f0e6bb8b2a687c662bc2e782fa0ce2
Federico Montejo <fmontejo@usf.edu>
2025-10-26 22:47:15 -0400

    Finished creting the counter after a long try.

---
+++ components /counter.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function Counter({initialCount = 0, initialStep =1}) {
+    const [count,setCount] = useState(initialCount);
+    const [step, setStep] = useState(initialStep);
+
+    function increase() {
+        setCount(count + step);
+    }
+
+    function decrease() {
+        if (count - step >= 0) {
+            setCount(count - step);
+        }
+    }
+
+    function reset() {
+        setCount(0);
+    }
+
+    return(
+        <> 
+        <h2>Count: {count}</h2>
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Dncrease</button>
+        <button onClick={reset}>Reset</button>
+        </>
+    );
+};
+
+export default Counter;
